0c46f0862724c2779025c7960d3fc1a38a73aa8a
Justin <fan.justin4.1@gmail.com>
2023-02-18 14:03:24 -0800

    map

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-function getLocation() {
-    var locationError = false;
-    const errorMessage = "Unable to retrieve your location. Please try again later.";
-
-    //requesting permission to access location
-    if (navigator.permissions) {
-        navigator.permissions.query({ name: 'geolocation' }).then(permission => {
-            if (permission.state === 'granted') {
-                // permission granted, call getCurrentPosition()
-                navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-            } else if (permission.state === 'prompt') {
-                //permission not granted, prompt the user to allow location access
-                alert("Please allow location access.");
-            } else {
-                // permission denied
-                console.log("User denied the request for location access.");
-            }
-        });
-    } else {
-        // navigator.permissions not supported, call getCurrentPosition()
-    }
-
-    //using user location data
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-    function successCallback(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
-        // use the latitude and longitude data
-        console.log("Latitude: ${latitude} °, Longitude: ${longitude} °");
-        var x = document.getElementById("demo");
-        x.innerHTML = "Latitude: " + latitude + " Longitude: " + longitude;
-
-    }
-
-    //handle errors
-    function errorCallback(error) {
-        locationError = true;
-        console.error("Error getting location data: ${error.message}");
-        //code to display error message to user on web page
+window.addEventListener('load', initMap);
 
 
-    }
+var latitude = 0, longitude = 0;
+var locationError = false;
+const errorMessage = "Unable to retrieve your location. Please try again later.";
+
+//requesting permission to access location
+if (navigator.permissions) {
+    navigator.permissions.query({ name: 'geolocation' }).then(permission => {
+        if (permission.state === 'granted') {
+            // permission granted, call getCurrentPosition()
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        } else if (permission.state === 'prompt') {
+            //permission not granted, prompt the user to allow location access
+            alert("Please allow location access.");
+        } else {
+            // permission denied
+            console.log("User denied the request for location access.");
+        }
+    });
+} else {
+    // navigator.permissions not supported, call getCurrentPosition()
+}
+
+//using user location data
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+function successCallback(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    // use the latitude and longitude data
+    console.log("Latitude: "+latitude+", Longitude: "+longitude);
+    var x = document.getElementById("demo");
+    x.innerHTML = "Latitude: " + latitude + " Longitude: " + longitude;
+    initMap();
+}
+
+//handle errors
+function errorCallback(error) {
+    locationError = true;
+    console.error("Error getting location data: ${error.message}");
+    //code to display error message to user on web page
+
+
+}
+
+
+function initMap() {
+    // Create a new map instance
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: latitude, lng: longitude },
+        zoom: 10
+    });
 }
