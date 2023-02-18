bec756d54b00b18758cc1c2c86a793f3b1271818
Justin <fan.justin4.1@gmail.com>
2023-02-18 11:42:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ if (navigator.permissions) {
     navigator.permissions.query({ name: 'geolocation' }).then(permission => {
         if (permission.state === 'granted') {
             // permission granted, call getCurrentPosition()
-
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+            console.log("permission granted");
         } else if (permission.state === 'prompt') {
-            // permission not granted, prompt the user to allow location access
-
+            //permission not granted, prompt the user to allow location access
+            alert("User denied the request for location access.");
 
         } else {
             // permission denied
-
+            alert("User denied the request for location access.");
         }
     });
 } else {
@@ -32,6 +33,24 @@ function errorCallback(error) {
 function errorCallback(error) {
     const errorMessage = "Unable to retrieve your location. Please try again later.";
     console.error(`Error getting location data: ${error.message}`);
-    // display error message to user
-    
+    //code to display error message to user on web page
+    alert(errorMessage);
+
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for location access.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
 }
